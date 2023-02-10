4b1db614044249f1a8b31c233e24ca5118af2ee3
Jazziel Puente <jazziel.puente.v@gmail.com>
2023-02-10 11:42:29 -0700

    Implementando JsonWebToken para la seguridad

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-  apiKey: process.env.API_KEY
+  jwtSecret: process.env.JWT_SECRET
 }
 
 module.exports = config
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
 
 router.post('/',
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
     try {
-      res.json(req.user)
+      const user = req.user
+      const payload = {
+        id: user.id,
+        role: user.role
+      }
+      const token = jwt.sign(payload, config.jwtSecret)
+      res.status(200).json({
+        message: 'success',
+        data: {
+          user,
+          token
+        }
+      })
     } catch (error) {
       next(error)
     }
